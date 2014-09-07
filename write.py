cbc862d3c2c619dd9072c1cc36abf0ef6700ff58
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2014-09-07 13:52:05 +0000

    Start by writing out paragraphs first

--- write.py
+++ write.py
@@ -10,13 +10,22 @@ def render(data):
         TEMPLATE_DIRS=('/vagrant/code/social-aggregator/templates',))
     django.setup()
     t = loader.get_template('output.html')
-    c = Context({'subme':'mememem'})
+    c = Context({'data':data})
     l = t.render(c)
     print l.strip('\n')   
+
+def process_for_output(posts):
+    processed_posts = []
+    for p in posts:
+        if 'selected_paragraph' in p:
+            processed_posts.append({
+                'country': p['country']
+                'paragraph': p['selected_paragraph']
+                })
+    return processed_posts
 
 if __name__ == '__main__':
     tumblr_posts = tumblr.get_tagged_posts('peacecorps')
     tumblr_posts = tumblr.select_posts(tumblr_posts)
-
-    data = {'tumblr': tumblr_posts}
-    render()
+    posts = process_for_output(tumblr_posts)
+    render(posts)
